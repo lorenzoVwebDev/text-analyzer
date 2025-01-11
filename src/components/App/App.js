cb9d75ec48b3821503b5c58c610de6bb54a1c155
lorenzoVwebDev <lorenzoviganego.work@libero.it>
2025-01-11 11:25:11 +0100

    updated

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,27 @@
+import { createContext, useState } from 'react';
 import { GitRepositoryHeader } from '../common';
 
+export const TextContext = createContext();
 function App() {
+  const [ text, setText ] = useState('');
+
   return (
-    <div className="App">
+    <TextContext.Provider
+      value={text}
+    >
       <GitRepositoryHeader/>
-    </div>
+      <div className="text-analyzer-wrapper">
+        <label htmlFor=""></label>
+        <textarea 
+          id="analyzer" 
+          name="analyzer"
+          rows="10"
+          cols="100"
+          onChange={e => {setText(e.target.value)}}
+        >
+        </textarea>
+      </div>
+      </TextContext.Provider>
   );
 }
 
